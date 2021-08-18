8c0699797c1ed35d5da5757388c6e5aeafac50de
leeanabel <lee.anabel18@gmail.com>
2021-08-18 16:33:04 +0800

    Edits

--- src/script.js
+++ src/script.js
@@ -8,13 +8,16 @@
 
 
 function displayTemperature(response){
+    console.log(response.data);
     let temperatureElement=document.querySelector("#temperature"); 
-    temperatureElement.innerHTML= response.data.main.temp;
+    temperatureElement.innerHTML= Math.round(response.data.main.temp);
+    let cityElement= document.querySelector("#city");
+    cityElement.innerHTML= response.data.name;
 }
 
 let apiKey = "915204bb13bbec134e32fb3c5dd5973e"; 
-let city = "New York";
 let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
 axios.get(apiUrl).then(displayTemperature);
 
 
