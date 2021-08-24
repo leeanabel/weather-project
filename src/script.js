d0325204b57e04ebe1db0a7fc0ff7a90964bb778
leeanabel <lee.anabel18@gmail.com>
2021-08-24 17:15:12 +0800

    Update

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+console.log("Hello");
+
 // function getCity(event){
 //     event.preventDefault();
 //     let value1= document.getElementById("city-input").value;
@@ -20,63 +22,62 @@ function getDate(){
     let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
     let day = days[date.getDay()]; 
 
-    let h2 = document.querySelector("#today");
     document.getElementById("today").innerHTML = `${day} ${hours}:${minutes}`; 
     
 }
 
-
 function displayTemperature(response){
     let temperatureElement=document.getElementById("temperature"); 
     temperatureElement.innerHTML= Math.round(response.data.main.temp);
-    let cityElement= document.getElementById("city");
-    cityElement.innerHTML= response.data.name;
     let precipitationElement= document.getElementById("description");
-    precipitationElement.innerHTML = response.data.weather[0];
+    precipitationElement.innerHTML = response.data.weather[0].description;
     let windElement = document.getElementById("wind-speed");
-    windElement.innerHTML = Math.round(responses.data.wind.speed); 
-   
+    windElement.innerHTML = Math.round(response.data.wind.speed); 
+    let city= document.getElementById("city"); 
+    cityElement.innerHTML= response.data.city.name;
+    
 }
 
-let apiKey = "915204bb13bbec134e32fb3c5dd5973e"; 
-let city= document.getElementById("city");
-let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+  let apiKey = "915204bb13bbec134e32fb3c5dd5973e"; 
+    let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+
 
 function searchCity(city){
-let apiKey = "915204bb13bbec134e32fb3c5dd5973e"; 
-let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
-}
-
-function searchLocation(position){
-    lon = position.coords.longitude;
-    lat = position.coords.latitude; 
-    let apiKey = "915204bb13bbec134e32fb3c5dd5973e"; 
-let apiUrl= `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-axios.get(apiUrl).then(displayTemperature); 
+document.getElementById("city").innerHTML= searchCity(city);
 }
 
 function getCurrentLocation(event){
     event.preventDefault();
-    navigator.geolocation.getCurrentLocation(searchLocation); 
+    navigator.geolocation.getCurrentLocation(showPosition); 
+}
+
+function showPosition(position){
+    lon = position.coords.longitude;
+    lat = position.coords.latitude; 
+    let apiKey = "915204bb13bbec134e32fb3c5dd5973e"; 
+    let apiUrl= `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    axios.get(apiUrl).then(getCurrentLocation); 
 }
 
 // Convert F -> C
 
-function convertFarenheitToCelsius(event){
-    event.preventDefault();
-    let temperature = document.querySelector("#temperature");
-    let F= document.querySelector(".farenheit");
-    F.addEventListener("click", convertFarenheitToCelsius);
+function convertFarenheitToCelsius(farenheit){
+    let temperature = document.getElementById("farenheit");
     return ((temperature - 32) * 5) /9 ;
 }
 
+    let F = document.getElementById("farenheit"); 
+    F.addEventListener("click", convertFarenheitToCelsius); 
+    document.querySelector(".farenheit").innerHTML = convertFarenheitToCelsius(); 
+    
+
 // Convert C -> F
 
-function convertCelsiustoFarenheit(event){
-    event.preventDefault();
-    let celsiusElement = document.querySelector("#temperature");
-    return (temperature * 9/5) +32 ); 
-    celsiusElement.addEventListener ("click", convertCelsiustoFarenheit);
+function convertCelsiustoFarenheit(celsius){
+    let temperature = document.getElementById("temperature");
+    return (temperature * 9/5) + 32 ; 
 }
+
+document.getElementById("temperature").innerHTML = convertCelsiustoFarenheit(); 
