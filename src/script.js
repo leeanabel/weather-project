e077a2812966b78b23094a3352d9a2a195efbf2b
leeanabel <lee.anabel18@gmail.com>
2021-08-25 14:36:56 +0800

    Added humidity

--- src/script.js
+++ src/script.js
@@ -33,6 +33,8 @@ function displayTemperature(response){
     windElement.innerHTML = Math.round(response.data.wind.speed); 
     let cityElement= document.getElementById("city"); 
     cityElement.innerHTML= response.data.name;
+    let humidityElement= document.querySelector("#humidity");
+    humidityElement.innerHTML = response.data.main.humidity;
     let iconElement=document.querySelector("#icon");
     iconElement.setAttribute ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`) ; 
     iconElement.setAttribute ("alt", response.data.weather[0].description); 
